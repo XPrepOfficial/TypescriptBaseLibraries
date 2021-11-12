d703b4d0b7ba68ce95086fbe02c95f113acf882f
Sarthak Garg <sarthak.garg@classplus.co>
2021-11-12 16:23:36 +0530

    build files updated

---
+++ build/bootloader/Redis/RedisConnection.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const redis_1 = __importDefault(require("redis"));
+class RedisConnection {
+    static getRedisConnection(port, host) {
+        try {
+            if (!this._instance) {
+                this._instance = new RedisConnection();
+            }
+            this.connectionObject = redis_1.default.createClient(port, host);
+            this.connectionObject.on('connect', function () {
+                console.log("Redis connection established successfully.");
+            });
+        }
+        catch (e) {
+            throw e.message;
+        }
+    }
+}
+exports.default = RedisConnection;
